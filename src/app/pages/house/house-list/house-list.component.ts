e1cc764cc5048c713ffe70fb9e454bc33cee7928
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-12-12 12:24:24 +0100

    added simple house list

--- src/app/pages/house/house-list/house-list.component.ts
+++ src/app/pages/house/house-list/house-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HouseService } from 'src/app/services/house.service';
+import { HouseSimpleOutput } from '../../../models/house.model';
 
 @Component({
   selector: 'app-house-list',
   templateUrl: './house-list.component.html',
-  styleUrls: ['./house-list.component.scss']
+  styleUrls: ['./house-list.component.scss'],
 })
 export class HouseListComponent implements OnInit {
+  houseList: HouseSimpleOutput[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private houseService: HouseService) {
+    houseService.getHouseList().subscribe((response: HouseSimpleOutput[]) => {
+      this.houseList = response;
+    });
   }
 
+  ngOnInit(): void {}
 }
