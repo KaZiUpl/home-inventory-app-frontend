a3a1d96a88d3e98e7d50184c71ec8b5e51f57ef3
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-12-13 13:28:21 +0100

    added item delete

--- src/app/pages/item/components/item-list/item-list.component.ts
+++ src/app/pages/item/components/item-list/item-list.component.ts
@@ -8,6 +8,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Subscription, pipe } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { OnDestroy } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { ItemService } from '../../../../services/item.service';
 import { ItemSimpleOutput } from '../../../../models/item.model';
@@ -32,56 +34,64 @@ export class ItemListComponent implements OnInit, OnDestroy {
   constructor(
     private itemService: ItemService,
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private snackBar: MatSnackBar
   ) {
     this.searchForm = new FormGroup({
       search: new FormControl(null, []),
     });
 
-    itemService.getItemList().subscribe((items: ItemSimpleOutput[]) => {
-      this.items = items;
+    itemService.getItemList().subscribe(
+      (items: ItemSimpleOutput[]) => {
+        this.items = items;
 
-      this.itemsDataSource = new MatTableDataSource(items);
-      this.itemsDataSource.sort = this.sort;
-      this.itemsDataSource.paginator = this.paginator;
+        this.itemsDataSource = new MatTableDataSource(items);
+        this.itemsDataSource.sort = this.sort;
+        this.itemsDataSource.paginator = this.paginator;
 
-      //check URL for paginator settings
-      this.activatedRoute.queryParamMap.subscribe((params: any) => {
-        //check for page number
-        if (params.params.page) {
-          this.itemsDataSource.paginator.pageIndex = params.params.page;
-        }
-
-        //check for page size
-        if (params.params.page_size) {
-          //check for page size in available page sizes
-          if (
-            this.pageSizes.filter((number) => {
-              return number == params.params.page_size;
-            }).length > 0
-          ) {
-            this.itemsDataSource.paginator.pageSize = params.params.page_size;
-            // this.currentURLPageSize = params.params.page_size;
-          } else {
-            //reset paginator
-            this.itemsDataSource.paginator.pageSize = this.pageSizes[0];
-            //reset page
-            this.router.navigate([], {
-              relativeTo: this.activatedRoute,
-              queryParams: {
-                page_size: this.itemsDataSource.paginator.pageSize,
-              },
-              queryParamsHandling: 'merge',
-            });
+        //check URL for paginator settings
+        this.activatedRoute.queryParamMap.subscribe((params: any) => {
+          //check for page number
+          if (params.params.page) {
+            this.itemsDataSource.paginator.pageIndex = params.params.page;
           }
-        }
-        // check for search filter
-        if (params.params.search) {
-          this.itemsDataSource.filter = params.params.search;
-          this.searchValue = params.params.search;
-        } else this.itemsDataSource.filter = null;
-      });
-    });
+
+          //check for page size
+          if (params.params.page_size) {
+            //check for page size in available page sizes
+            if (
+              this.pageSizes.filter((number) => {
+                return number == params.params.page_size;
+              }).length > 0
+            ) {
+              this.itemsDataSource.paginator.pageSize = params.params.page_size;
+              // this.currentURLPageSize = params.params.page_size;
+            } else {
+              //reset paginator
+              this.itemsDataSource.paginator.pageSize = this.pageSizes[0];
+              //reset page
+              this.router.navigate([], {
+                relativeTo: this.activatedRoute,
+                queryParams: {
+                  page_size: this.itemsDataSource.paginator.pageSize,
+                },
+                queryParamsHandling: 'merge',
+              });
+            }
+          }
+          // check for search filter
+          if (params.params.search) {
+            this.itemsDataSource.filter = params.params.search;
+            this.searchValue = params.params.search;
+          } else this.itemsDataSource.filter = null;
+        });
+      },
+      (error: HttpErrorResponse) => {
+        this.snackBar.open(error.error.message, null, {
+          duration: 3000,
+        });
+      }
+    );
   }
 
   ngOnInit(): void {
@@ -89,11 +99,16 @@ export class ItemListComponent implements OnInit, OnDestroy {
     this.searchFieldSub = this.searchForm.controls['search'].valueChanges
       .pipe(debounceTime(500), distinctUntilChanged())
       .subscribe((value: string) => {
-        this.itemService
-          .getItemList({ name: value })
-          .subscribe((response: any) => {
+        this.itemService.getItemList({ name: value }).subscribe(
+          (response: any) => {
             this.itemsDataSource.data = response;
-          });
+          },
+          (error: HttpErrorResponse) => {
+            this.snackBar.open(error.error.message, null, {
+              duration: 3000,
+            });
+          }
+        );
         this.searchValue = value;
       });
   }
@@ -107,5 +122,21 @@ export class ItemListComponent implements OnInit, OnDestroy {
       queryParams: { page_size: event.pageSize, page: event.pageIndex },
       queryParamsHandling: 'merge',
     });
+  }
+
+  onItemDelete(id: string): void {
+    this.itemService.deleteItem(id).subscribe(
+      (response: any) => {
+        //delete item from datasource
+        this.itemsDataSource.data = this.itemsDataSource.data.filter(
+          (item) => item._id != id
+        );
+      },
+      (error: HttpErrorResponse) => {
+        this.snackBar.open(error.error.message, null, {
+          duration: 3000,
+        });
+      }
+    );
   }
 }
