5b41beb4397055f3d12006bc6e6050131efdb9a1
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-12-29 14:01:19 +0100

    added custom error state matcher for password confirmation during sign up process

--- src/app/pages/auth/components/register/register.component.ts
+++ src/app/pages/auth/components/register/register.component.ts
@@ -1,10 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  NgForm,
+  FormGroupDirective,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { TokenOutput } from 'src/app/models/token.model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+class CrossFieldErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    return (
+      (control.dirty && form.hasError('passwordsNotEqual')) ||
+      (form.submitted && control.invalid) ||
+      (control.touched && control.hasError('required'))
+    );
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -15,22 +35,30 @@ export class RegisterComponent implements OnInit {
   hidePassword: boolean = true;
   registerForm: FormGroup;
 
+  crossFieldErrorMatcher = new CrossFieldErrorStateMatcher();
+
   constructor(
     private userService: UserService,
     private router: Router,
     private snackBarService: MatSnackBar
   ) {
-    this.registerForm = new FormGroup({
-      login: new FormControl(null, [Validators.required]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required]),
-    });
+    this.registerForm = new FormGroup(
+      {
+        login: new FormControl(null, [Validators.required]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required]),
+        confirmPassword: new FormControl(null, [Validators.required]),
+      },
+      [this.passwordsNotEqual]
+    );
   }
 
   ngOnInit(): void {}
 
   onRegisterFormSubmit() {
     if (this.registerForm.invalid) {
+      console.log(this.registerForm);
+
       return;
     }
 
@@ -64,5 +92,13 @@ export class RegisterComponent implements OnInit {
           });
         }
       );
+  }
+
+  passwordsNotEqual(signUpForm: FormGroup) {
+    const condition =
+      signUpForm.get('password').value !==
+      signUpForm.get('confirmPassword').value;
+
+    return condition ? { passwordsNotEqual: true } : null;
   }
 }
