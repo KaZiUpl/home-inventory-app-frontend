d63b5f7f351d2b99c7304442daabea1b40827be4
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2021-01-26 13:03:36 +0100

    changed apiUrl into localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://192.168.11.26:3000',
-  apiUrl: 'https://home-inventory-app.herokuapp.com',
+  apiUrl: 'https://localhost:3000',
 };
